a706adc0c400d311fb6f4acd32dff8ecf3a187d8
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-03-20 15:26:30 +0700

    fix(races): set player dimension back to 0 on leaving race

--- src/Proton.Shared/Constants/Route.cs
+++ src/Proton.Shared/Constants/Route.cs
@@ -9,7 +9,6 @@ public sealed class Route
         Value = value;
     }
 
-    public static readonly Route RaceMainMenu = new("race-main-menu");
     public static readonly Route RaceCreator = new("race-creator");
     public static readonly Route RaceMainMenuList = new("racing_menu_list");
     public static readonly Route Auth = new("auth");
