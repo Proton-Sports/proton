7eb0960579131cf4b1de494dc4da0eb8e36b3271
Dan G <134007909+dGxxxx@users.noreply.github.com>
2024-04-04 15:09:37 +0300

    use await using for dbContext

--- src/Proton.Server.Infrastructure/CharacterCreator/CharacterHandler.cs
+++ src/Proton.Server.Infrastructure/CharacterCreator/CharacterHandler.cs
@@ -8,25 +8,25 @@ public class CharacterHandler(IDbContextFactory<DefaultDbContext> defaultDbFacto
 {
     public async Task<bool> ExistsCharacter(int characterId)
     {
-        var defaultDb = await defaultDbFactory.CreateDbContextAsync();
+        await using var defaultDb = await defaultDbFactory.CreateDbContextAsync();
         return await defaultDb.Characters.AnyAsync(x => x.Id == characterId);
     }
 
     public async Task<bool> HasCharacter(long userId)
     {
-        var defaultDb = await defaultDbFactory.CreateDbContextAsync();
+        await using var defaultDb = await defaultDbFactory.CreateDbContextAsync();
         return await defaultDb.Characters.AnyAsync(x => x.Id == userId);
     }
     
     public async Task<Character?> GetByUserId(long userId)
     {
-        var defaultDb = await defaultDbFactory.CreateDbContextAsync();
+        await using var defaultDb = await defaultDbFactory.CreateDbContextAsync();
         return await defaultDb.Characters.FirstOrDefaultAsync(x => x.Id == userId);
     }
     
     public async Task Add(Character userCharacter)
     {
-        var defaultDb = await defaultDbFactory.CreateDbContextAsync();
+        await using var defaultDb = await defaultDbFactory.CreateDbContextAsync();
         defaultDb.Characters.Add(userCharacter);
         await defaultDb.SaveChangesAsync();
     }
