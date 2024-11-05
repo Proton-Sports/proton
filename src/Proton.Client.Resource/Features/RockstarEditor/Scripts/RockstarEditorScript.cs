7daff799d3294150137aa11d9f58275067596170
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-11-06 03:19:41 +0700

    feat: add rockstar editor keybindings

--- src/Proton.Client.Resource/Features/RockstarEditor/Scripts/RockstarEditorScript.cs
+++ src/Proton.Client.Resource/Features/RockstarEditor/Scripts/RockstarEditorScript.cs
@@ -1,4 +1,5 @@
 using AltV.Net.Client;
+using AltV.Net.Client.Elements.Data;
 using Proton.Client.Resource.Commons;
 
 namespace Proton.Client.Resource.Features.RockstarEditor.Scripts;
@@ -6,34 +7,44 @@ namespace Proton.Client.Resource.Features.RockstarEditor.Scripts;
 public sealed class RockstarEditorScript : HostedService
 {
     private uint interval;
+    private bool recording;
 
     public override Task StartAsync(CancellationToken ct)
     {
-        Alt.OnConsoleCommand += OnConsoleCommand;
+        Alt.OnKeyUp += OnKeyUp;
         return Task.CompletedTask;
     }
 
-    private void OnConsoleCommand(string command, string[] args)
+    private void OnKeyUp(Key key)
     {
-        switch (command)
+        switch (key)
         {
-            case "start":
-                Alt.Natives.StartReplayRecording(1);
-                break;
-            case "save":
-                var result = Alt.Natives.SaveReplayRecording();
-                Alt.Natives.StopReplayRecording();
-                break;
-            case "editor":
-                Alt.Natives.ActivateRockstarEditor(1);
-                interval = Alt.SetInterval(() =>
+            case Key.F9:
+                if (recording)
                 {
-                    if (Alt.Natives.IsScreenFadedOut())
+                    var result = Alt.Natives.SaveReplayRecording();
+                    Alt.Natives.StopReplayRecording();
+                    Alt.Log($"Save replay recording result: {result}.");
+                }
+                else
+                {
+                    Alt.Natives.StartReplayRecording(1);
+                }
+                recording = !recording;
+                break;
+            case Key.F10:
+                Alt.Natives.ActivateRockstarEditor(1);
+                interval = Alt.SetInterval(
+                    () =>
                     {
-                        Alt.Natives.DoScreenFadeIn(1000);
-                        Alt.ClearInterval(interval);
-                    }
-                }, 1000);
+                        if (Alt.Natives.IsScreenFadedOut())
+                        {
+                            Alt.Natives.DoScreenFadeIn(1000);
+                            Alt.ClearInterval(interval);
+                        }
+                    },
+                    1000
+                );
                 break;
         }
     }
