4b0ab75323f92e39e40d4aeb7a7b823074d6aa01
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-12-08 19:50:10 +0700

    feat(speedometer): attempt to support gamepad controls

--- src/Proton.Client.Resource/Utils/Scripts/SpeedometerScript.cs
+++ src/Proton.Client.Resource/Utils/Scripts/SpeedometerScript.cs
@@ -4,94 +4,131 @@ using Proton.Client.Resource.Features.UiViews.Abstractions;
 using Proton.Shared.Constants;
 using Proton.Shared.Interfaces;
 
-namespace Proton.Client.Resource.Utils.Scripts
+namespace Proton.Client.Resource.Utils.Scripts;
+
+public sealed class SpeedometerScript : IStartup
 {
-    internal class SpeedometerScript : IStartup
+    private const int INPUT_VEH_ACCELERATE = 71;
+    private const int INPUT_VEH_HANDBRAKE = 76;
+
+    private readonly IUiView uiView;
+    private uint updateIntervalId;
+    private int brakeCount;
+
+    public SpeedometerScript(IUiView uiView)
     {
-        private readonly IUiView uiView;
-        private uint updateIntervalId;
-        private int brakeCount;
+        Alt.OnPlayerEnterVehicle += Alt_OnPlayerEnterVehicle;
+        Alt.OnPlayerLeaveVehicle += Alt_OnPlayerLeaveVehicle;
+        this.uiView = uiView;
 
-        public SpeedometerScript(IUiView uiView)
+        Alt.Natives.HideHudComponentThisFrame(6);
+        Alt.Natives.HideHudComponentThisFrame(7);
+        Alt.Natives.HideHudComponentThisFrame(8);
+        Alt.Natives.HideHudComponentThisFrame(9);
+    }
+
+    private void Alt_OnPlayerEnterVehicle(AltV.Net.Client.Elements.Interfaces.IVehicle vehicle, byte seat)
+    {
+        Alt.Natives.HideHudComponentThisFrame(6);
+        Alt.Natives.HideHudComponentThisFrame(7);
+        Alt.Natives.HideHudComponentThisFrame(8);
+        Alt.Natives.HideHudComponentThisFrame(9);
+
+        uiView.Mount(Route.Speedometer);
+        updateIntervalId = Alt.SetInterval(UpdateVehicleUi, 50);
+
+        if (Alt.Natives.IsUsingKeyboardAndMouse(0))
         {
-            Alt.OnPlayerEnterVehicle += Alt_OnPlayerEnterVehicle;
-            Alt.OnPlayerLeaveVehicle += Alt_OnPlayerLeaveVehicle;
-            this.uiView = uiView;
-
-            Alt.Natives.HideHudComponentThisFrame(6);
-            Alt.Natives.HideHudComponentThisFrame(7);
-            Alt.Natives.HideHudComponentThisFrame(8);
-            Alt.Natives.HideHudComponentThisFrame(9);
-        }
-
-        private void Alt_OnPlayerEnterVehicle(AltV.Net.Client.Elements.Interfaces.IVehicle vehicle, byte seat)
-        {
-            Alt.Natives.HideHudComponentThisFrame(6);
-            Alt.Natives.HideHudComponentThisFrame(7);
-            Alt.Natives.HideHudComponentThisFrame(8);
-            Alt.Natives.HideHudComponentThisFrame(9);
-
-            uiView.Mount(Route.Speedometer);
-            updateIntervalId = Alt.SetInterval(UpdateVehicleUi, 50);
-
             Alt.OnKeyDown += OnKeyDown;
             Alt.OnKeyUp += OnKeyUp;
         }
-
-        private void Alt_OnPlayerLeaveVehicle(AltV.Net.Client.Elements.Interfaces.IVehicle vehicle, byte seat)
+        else
         {
-            uiView.Unmount(Route.Speedometer);
-            Alt.ClearInterval(updateIntervalId);
+            Alt.OnTick += OnTick;
+        }
+    }
+
+    private void Alt_OnPlayerLeaveVehicle(AltV.Net.Client.Elements.Interfaces.IVehicle vehicle, byte seat)
+    {
+        uiView.Unmount(Route.Speedometer);
+        Alt.ClearInterval(updateIntervalId);
+        if (Alt.Natives.IsUsingKeyboardAndMouse(0))
+        {
             Alt.OnKeyDown -= OnKeyDown;
             Alt.OnKeyUp -= OnKeyUp;
         }
-
-        private void UpdateVehicleUi()
+        else
         {
-            if (Alt.LocalPlayer.Vehicle == null)
-            {
-                return;
-            }
+            Alt.OnTick -= OnTick;
+        }
+    }
 
-            var vehicle = Alt.LocalPlayer.Vehicle;
-
-            uiView.Emit("vehicle:gear", vehicle.Gear);
-            uiView.Emit("vehicle:speed", (int)Alt.Natives.GetEntitySpeed(vehicle) * 3, 6);
-            uiView.Emit("vehicle:rpm", vehicle.Rpm * 10_000);
+    private void UpdateVehicleUi()
+    {
+        if (Alt.LocalPlayer.Vehicle == null)
+        {
+            return;
         }
 
-        private void OnKeyDown(Key key)
+        var vehicle = Alt.LocalPlayer.Vehicle;
+
+        uiView.Emit("vehicle:gear", vehicle.Gear);
+        uiView.Emit("vehicle:speed", (int)Alt.Natives.GetEntitySpeed(vehicle) * 3, 6);
+        uiView.Emit("vehicle:rpm", vehicle.Rpm * 10_000);
+    }
+
+    private void OnKeyDown(Key key)
+    {
+        switch (key)
         {
-            switch (key)
-            {
-                case Key.S:
-                case Key.Space:
-                    if (++brakeCount > 0)
-                    {
-                        uiView.Emit("vehicle:red", true);
-                    }
-                    break;
-                case Key.W:
-                    uiView.Emit("vehicle:green", true);
-                    break;
-            }
+            case Key.S:
+            case Key.Space:
+                if (++brakeCount > 0)
+                {
+                    uiView.Emit("vehicle:red", true);
+                }
+                break;
+            case Key.W:
+                uiView.Emit("vehicle:green", true);
+                break;
+        }
+    }
+
+    private void OnKeyUp(Key key)
+    {
+        switch (key)
+        {
+            case Key.S:
+            case Key.Space:
+                if (--brakeCount == 0)
+                {
+                    uiView.Emit("vehicle:red", false);
+                }
+                break;
+            case Key.W:
+                uiView.Emit("vehicle:green", false);
+                break;
+        }
+    }
+
+    private void OnTick()
+    {
+        if (Alt.Natives.IsControlJustPressed(0, INPUT_VEH_HANDBRAKE))
+        {
+            uiView.Emit("vehicle:red", true);
+        }
+        else if (Alt.Natives.IsControlJustReleased(0, INPUT_VEH_HANDBRAKE))
+        {
+            uiView.Emit("vehicle:red", false);
         }
 
-        private void OnKeyUp(Key key)
+        if (Alt.Natives.IsControlJustPressed(0, INPUT_VEH_ACCELERATE))
         {
-            switch (key)
-            {
-                case Key.S:
-                case Key.Space:
-                    if (--brakeCount == 0)
-                    {
-                        uiView.Emit("vehicle:red", false);
-                    }
-                    break;
-                case Key.W:
-                    uiView.Emit("vehicle:green", false);
-                    break;
-            }
+            uiView.Emit("vehicle:green", true);
+        }
+        else if (Alt.Natives.IsControlJustReleased(0, INPUT_VEH_ACCELERATE))
+        {
+            uiView.Emit("vehicle:green", false);
         }
     }
 }
